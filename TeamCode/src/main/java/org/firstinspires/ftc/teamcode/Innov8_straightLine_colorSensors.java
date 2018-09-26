b8e25fa3b98b095f3ee59e91927cf71dcbf0d515
lowerKase <kaseyicecream@gmail.com>
2018-09-25 20:35:18 -0500

    We changed the code to match the pseudocode and edited the if and while loops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Innov8_straightLine_colorSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Innov8_straightLine_colorSensors.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class Innov8_straightLine_colorSensors extends LinearOpMode {
 
     /* Declare OpMode members. */
-    HardwareInnov8Duck robot = new HardwareInnov8Duck();   // Use a Innov8's hardware
+    HardwareInnov8Duck robot = new HardwareInnov8Duck();   // Use Duck's hardware
 
     /*Declare variables */
     double startPositionR = 0;
@@ -22,10 +22,10 @@ public class Innov8_straightLine_colorSensors extends LinearOpMode {
     int FDrive = 2;
     int teamColor = 0;   //0 = red   or 1 = blue
     int taskNumber = 0;   //used to determine the step that should be executed
-    double multR = 0.02;
+    double multR = 0.1;
     double multL = 0.02;
-    double correctL = 1;
-    double correctR = 1;
+    double correctL = 1;  // 1 or -1
+    double correctR = 1;  //1 or -1
 
     public void runOpMode() throws InterruptedException {
 
@@ -39,40 +39,51 @@ public class Innov8_straightLine_colorSensors extends LinearOpMode {
         telemetry.addData("CurrentL", robot.leftMotor.getCurrentPosition());
         telemetry.addData("EndR", endPositionR);
         telemetry.addData("EndL", endPositionL);
+        telemetry.addData("RightPower",robot.rightMotor.getPower());
+        telemetry.addData("LeftPower",robot.leftMotor.getPower());
         telemetry.update();
+
 
 
         waitForStart();
 
 
         while (opModeIsActive() && taskNumber != 9999) {
+            telemetry.addData("Case", taskNumber);
+            telemetry.addData("RightRed", robot.rightFruity.red());
+            telemetry.addData("RightBlue", robot.rightFruity.blue());
+            telemetry.addData("right", robot.rightMotor.getCurrentPosition());
+            telemetry.addData("left", robot.leftMotor.getCurrentPosition());
+            telemetry.addData("startR", startPositionR);
+            telemetry.addData("endR", endPositionR);
+            telemetry.addData("LeftRed", robot.leftFruity.red());
+            telemetry.addData("LeftBlue", robot.leftFruity.blue());
+            telemetry.addData("startL", startPositionL);
+            telemetry.addData("endL", endPositionL);
+            telemetry.addData("RightPower",robot.rightMotor.getPower());
+            telemetry.addData("LeftPower",robot.leftMotor.getPower());
+            telemetry.update();
 
-                if (robot.rightFruity.red() <= 450) {
-                        robot.rightMotor.setPower(10 * multR * correctR);
-                        telemetry.addData("Case", taskNumber);
-                        telemetry.addData("right", robot.rightMotor.getCurrentPosition());
-                        telemetry.addData("left", robot.leftMotor.getCurrentPosition());
-                        telemetry.addData("startL", startPositionL);
-                        telemetry.addData("endL", endPositionL);
-                        telemetry.update();
-                    }
-                    else{
-            robot.rightMotor.setPower(0);
-        }
-                if (robot.leftFruity.red() <= 300) {
-                    robot.leftMotor.setPower(10 * multL * correctL);
-                    telemetry.addData("Case", taskNumber);
-                    telemetry.addData("right", robot.rightMotor.getCurrentPosition());
-                    telemetry.addData("left", robot.leftMotor.getCurrentPosition());
-                    telemetry.addData("startL", startPositionL);
-                    telemetry.addData("endL", endPositionL);
-                    telemetry.update();
+            robot.rightMotor.setPower(10 * multR * correctR);
+            robot.leftMotor.setPower(10 * multL * correctL);
+
+            while  (robot.leftMotor.getPower() != 0 || robot.rightMotor.getPower() != 0)  {
+
+                telemetry.update();
+
+                if (robot.rightFruity.red() >= 450) {
+
+                    robot.rightMotor.setPower(0);
                 }
 
-                else {
-                    robot.leftMotor.setPower(0);
+                if (robot.leftFruity.red() >= 300) {
 
-                taskNumber = 9999;
+                    robot.leftMotor.setPower(0);
+                }
+
+
+
+            taskNumber = 9999;
             }
         }
     }
